94d30f0b6914af884da3c245536dcb959de74118
kokila <kokila.alu@gmail.com>
2015-08-28 11:23:44 +0530

    Room Checkin UI created

--- src/main/webapp/resources-new/dist/js/redberry.js
+++ src/main/webapp/resources-new/dist/js/redberry.js
@@ -3,32 +3,35 @@ function SideBarViewModel(){
     self.menuItems = [
         {name:'dashboard',icon:'fa-home',dispalyName:'DashBoard'},
         {name:'checkin',icon:'fa-shopping-cart',dispalyName:'Check In'},
-        {name:'checkin',icon:'fa-shopping-cart',dispalyName:'Check In'},
         {name:'employee',icon:'fa-eye',dispalyName:'Employee'}];
 
     self.chosenFolderId = ko.observable();
     self.chosenFolderData = ko.observable();
 
     // Behaviours
-    self.goToFolder = self.goToFolder = function(menuItem) { location.hash = menuItem.name };
+    self.goToFolder = self.goToFolder = function(menuItem) {
+        location.hash = menuItem.name
+    };
 
     // Client-side routes
     Sammy(function() {
-        this.get('#:menuItem', function() {
-            self.chosenFolderId(this.params.menuItem);
+        this.get('/#:menuHash', function() {
 
-            var item = this.params.menuItem;
-
-            $.get('templates/'+item+'.html', function(uidata) {
+            var menuHash = this.params.menuHash;
+            var checkMenu = $.grep(self.menuItems, function (n) { return n.name == menuHash });
+            if(checkMenu.length){
+            $.get('templates/'+menuHash+'.html', function(uidata) {
+                self.chosenFolderId(menuHash);
                 $('#template_holder').html(uidata);
                 var vm = ko.dataFor(document.getElementById('content'));
                 if( vm ) {
                     ko.cleanNode(document.getElementById('content'));
-                    ko.applyBindings(eval(item+'ViewModel'), document.getElementById('content'));
+                    ko.applyBindings(eval(menuHash+'ViewModel'), document.getElementById('content'));
                 }else{
-                    ko.applyBindings(eval(item+'ViewModel'),document.getElementById('content'));
+                    ko.applyBindings(eval(menuHash+'ViewModel'),document.getElementById('content'));
                 }
             });
+            }
         });
 
         this.get('', function() { this.app.runRoute('get', '#dashboard') });
@@ -39,6 +42,17 @@ ko.applyBindings(sidebar,document.getElementById('admin-sidebar'));
 ko.applyBindings(sidebar,document.getElementById('content-header'));
 
 
+var Room = function(no, show) {
+    var self = this;
+    self.no = ko.observable(no);
+    self.category = ko.observableArray(['dulux','supper','luxury']);
+    self.AC = ko.observableArray(['NO','YES']);
+    self.occupancy = ko.observableArray(['yes','no']);
+    self.mealConfig = ko.observableArray(['Vegetarian', 'non vege']);
+    self.show = ko.observable(show);
+};
+
+
 
 var checkinViewModel = {
     checkInToOut: ko.observable(),
@@ -47,20 +61,42 @@ var checkinViewModel = {
     nic: ko.observable(),
     email: ko.observable(),
     telephone: ko.observable(),
-    Rooms: ko.observableArray([]),
+    noOfRooms: ko.observable(1),
+
+
+    Rooms: ko.observableArray([
+        new Room(1,true)
+    ]),
 
     TravelSources: ko.observableArray(['none','Lanka Tours','Sri Tours','Sl Tour Guide','Jhon Snow']),
-    category: ko.observableArray(['dulux','supper','luxury']),
-    ac: ko.observableArray(['none','Lanka Tours','Sri Tours','Sl Tour Guide','Jhon Snow']),
-    meal: ko.observableArray(['none','Lanka Tours','Sri Tours','Sl Tour Guide','Jhon Snow']),
-    occupancy: ko.observableArray(['none','Lanka Tours','Sri Tours','Sl Tour Guide','Jhon Snow']),
 
-    initCheckIn:function(){
+
+    initialiseUI:function(){
         $('#checkInToOut').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+
+    },
+
+
+    roomSelect: function(room){
+        var r = ko.utils.arrayFirst(this.Rooms, function(r) {
+            return r.no == room.no;
+        });
+
+        if (r.show) {
+            alert(r.show);
+            r.show(false);
+        }else{
+            r.show(true);
+        }
     },
 
     goToSecondStep: function(){
-        $('#checkinTabs a[href="#tab_2"]').tab('show')
+        this.Rooms([new Room(1,true)]);
+        $('#checkinTabs a[href="#tab_2"]').tab('show');
+        for(i=2;i<=this.noOfRooms();i++){
+            //this.Rooms.push({no: i, category: ['dulux','supper','luxury'] , AC: ['NO','YES'], occupancy:['yes','no'] ,mealConfig:['Vegetarian', 'non vege'],show:false});
+            this.Rooms.push(new Room(i,false));
+        }
     }
 };
 
@@ -71,7 +107,8 @@ var dashboardViewModel = {
         { name: 'Autumn', months: [ 'September', 'October', 'November' ] },
         { name: 'Winter', months: [ 'December', 'January', 'February' ] }
     ]),
-    initCheckIn:function(){
-        $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+    initialiseUI:function(){
+
     }
 };
+
