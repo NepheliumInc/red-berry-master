968f635facb84eb168c8a8ef480586a407c7b989
kokila <kokila.alu@gmail.com>
2015-10-04 18:16:14 +0530

    sidebar accordion menu event delegation fixed

--- src/main/webapp/resources-new/dist/js/redberry.js
+++ src/main/webapp/resources-new/dist/js/redberry.js
@@ -1,3 +1,252 @@
+
+ko.observableArray.fn.subscribeArrayChanged = function(addCallback, deleteCallback) {
+    var previousValue = undefined;
+    this.subscribe(function(_previousValue) {
+        previousValue = _previousValue.slice(0);
+    }, undefined, 'beforeChange');
+    this.subscribe(function(latestValue) {
+        var editScript = ko.utils.compareArrays(previousValue, latestValue);
+        for (var i = 0, j = editScript.length; i < j; i++) {
+            switch (editScript[i].status) {
+                case "retained":
+                    break;
+                case "deleted":
+                    if (deleteCallback)
+                        deleteCallback(editScript[i].value);
+                    break;
+                case "added":
+                    if (addCallback)
+                        addCallback(editScript[i].value);
+                    break;
+            }
+        }
+        previousValue = undefined;
+    });
+};
+
+
+// Person object
+var Person = function(data, dt) {
+    this.id    = data.id;
+    this.first = ko.observable(data.first);
+    this.last  = ko.observable(data.last);
+    this.age   = ko.observable(data.age);
+    this.full  = ko.computed(function() {
+        return this.first() + " " + this.last();
+    }, this);
+
+    // Subscribe a listener to the observable properties for the table
+    // and invalidate the DataTables row when they change so it will redraw
+    var that = this;
+    $.each( [ 'first', 'last', 'age' ], function (i, prop) {
+        that[ prop ].subscribe( function (val) {
+            // Find the row in the DataTable and invalidate it, which will
+            // cause DataTables to re-read the data
+            var rowIdx = dt.column( 0 ).data().indexOf( that.id );
+            dt.row( rowIdx ).invalidate();
+        } );
+    } );
+};
+
+
+
+// Initial data set
+var data = [
+    { id: 0, first: "Allan", last: "Jardine", age: 86 },
+    { id: 1, first: "Bob", last: "ghj", age: 54 },
+    { id: 2, first: "Bob", last: "kl", age: 54 },
+    { id: 3, first: "er", last: "dfg", age: 54 },
+    { id: 4, first: "dfg", last: "ghj", age: 54 },
+    { id: 5, first: "sfsd", last: "rty", age: 54 },
+    { id: 6, first: "Bob", last: "cv", age: 54 },
+    { id: 7, first: "sdf", last: "fgh", age: 54 },
+    { id: 8, first: "Bob", last: "bvb", age: 54 },
+    { id: 9, first: "sdf", last: "nnmb", age: 54 },
+    { id: 10, first: "er", last: "dfg", age: 54 },
+    { id: 11, first: "dfg", last: "ghj", age: 54 },
+    { id: 12, first: "sfsd", last: "rty", age: 54 },
+    { id: 13, first: "Bob", last: "cv", age: 54 },
+    { id: 14, first: "sdf", last: "fgh", age: 54 },
+    { id: 15, first: "Bob", last: "bvb", age: 54 },
+    { id: 16, first: "sdf", last: "nnmb", age: 54 },
+    { id: 17, first: "Bob", last: "bnnn", age: 54 },
+    { id: 18, first: "Bob", last: "hjj", age: 54 }
+];
+
+
+
+// CheckOuts object
+var CheckOut = function(data, dt) {
+    this.id    = data.id;
+    this.CheckOutTime = ko.observable(data.CheckOutTime);
+    this.CheckOutDate = ko.observable(data.CheckOutDate);
+    this.CustomerName  = ko.observable(data.CustomerName);
+    this.NumberOfRooms   = ko.observable(data.NumberOfRooms);
+    this.Charge  = ko.observable(data.Charge);
+
+    // Subscribe a listener to the observable properties for the table
+    // and invalidate the DataTables row when they change so it will redraw
+    var that = this;
+    $.each( [ 'CheckOutTime','CheckOutDate', 'CustomerName', 'NumberOfRooms', 'Charge' ], function (i, prop) {
+        that[ prop ].subscribe( function (val) {
+            // Find the row in the DataTable and invalidate it, which will
+            // cause DataTables to re-read the data
+            var rowIdx = dt.column( 0 ).data().indexOf( that.id );
+            dt.row( rowIdx ).invalidate();
+        } );
+    } );
+};
+
+
+// Initial data set
+var data2 = [
+    { id: 0, CheckOutTime: "00630h", CheckOutDate: "13th November 2015", CustomerName: "Jardine", NumberOfRooms: 2 , Charge : "1200.00"},
+    { id: 1, CheckOutTime: "00730h", CheckOutDate: "13th November 2015", CustomerName: "dfgdfg", NumberOfRooms: 2 , Charge : "1900.00"},
+    { id: 2, CheckOutTime: "00830h", CheckOutDate: "13th November 2015", CustomerName: "kyjhkjk", NumberOfRooms: 3 , Charge : "12000.00"},
+    { id: 3, CheckOutTime: "00930h", CheckOutDate: "13th November 2015", CustomerName: "ghjghj", NumberOfRooms: 1 , Charge : "7200.00"},
+    { id: 4, CheckOutTime: "01030h", CheckOutDate: "13th November 2015", CustomerName: "nvbn", NumberOfRooms: 1 , Charge : "8200.00"}
+];
+
+
+
+$(document).ready(function() {
+    var people = ko.mapping.fromJS( [] );
+    var dt = $('#example').DataTable( {
+        "scrollY":        "100px",
+        "scrollCollapse": true,
+        "dom": 'rt',
+        "paging":         false,
+        columns: [
+            { data: 'id' },
+            { data: 'first()' },
+            { data: 'age()' }
+        ]
+    } );
+
+    $('#myInputTextField').keyup(function(){
+        dt.search($(this).val()).draw() ;
+    });
+
+    // Update the table when the `people` array has items added or removed
+    people.subscribeArrayChanged(
+        function ( addedItem ) {
+            dt.row.add( addedItem ).draw();
+        },
+        function ( deletedItem ) {
+            var rowIdx = dt.column( 0 ).data().indexOf( deletedItem.id );
+            dt.row( rowIdx ).remove().draw();
+        }
+    );
+
+    // Convert the data set into observable objects, and will also add the
+    // initial data to the table
+    ko.mapping.fromJS(
+        data,
+        {
+            key: function(data) {
+                return ko.utils.unwrapObservable(data.id);
+            },
+            create: function(options) {
+                return new Person(options.data, dt);
+            }
+        },
+        people
+    );
+
+
+
+
+   // CheckOuts
+
+
+
+
+    var CheckOuts = ko.mapping.fromJS( [] );
+    var CheckOutsdt = $('#CheckOuts').DataTable( {
+        "scrollY":        "100px",
+        "scrollCollapse": true,
+        "dom": 'rt',
+        "paging":         false,
+        columns: [
+            { data: 'CheckOutTime()' },
+            { data: 'CheckOutDate()' },
+            { data: 'CustomerName()' },
+            { data: 'NumberOfRooms()' },
+            { data: 'Charge()' }
+        ],
+        "columnDefs": [
+            {
+                "render": function ( data, type, row ) {
+                    return '<span class="redberry-datetime"><p>' + data +'</p>'+ row.CheckOutDate() + '</span>';
+                },
+                "targets": 0
+            },
+            { "visible": false,  "targets": [ 1 ] }
+        ]
+    } );
+
+    $('#CheckOutsSearch').keyup(function(){
+        CheckOutsdt.search($(this).val()).draw() ;
+    });
+
+    // Update the table when the `people` array has items added or removed
+    CheckOuts.subscribeArrayChanged(
+        function ( addedItem ) {
+            CheckOutsdt.row.add( addedItem ).draw();
+        },
+        function ( deletedItem ) {
+            var rowIdx = CheckOutsdt.column( 0 ).data().indexOf( deletedItem.id );
+            CheckOutsdt.row( rowIdx ).remove().draw();
+        }
+    );
+
+    // Convert the data set into observable objects, and will also add the
+    // initial data to the table
+    ko.mapping.fromJS(
+        data2,
+        {
+            key: function(data) {
+                return ko.utils.unwrapObservable(data.id);
+            },
+            create: function(options) {
+                return new CheckOut(options.data, CheckOutsdt);
+            }
+        },
+        CheckOuts
+    );
+
+
+    /*
+    // Examples:
+
+    // Update a field
+    people()[1].first( 'Allan3' );
+
+    // Add an item
+    people.push( new Person( {
+        id: 19,
+        first: "John",
+        last: "Smith",
+        age: 34
+    }, dt ) );
+
+    // Remove an item
+    people.shift();*/
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* #Employee */
 
 /*function SideBarViewModel(){
