8e98b6934c854ee844b7209f260cf12d3013d5f0
lakvith91 <lakshanvithana@gmail.com>
2015-10-08 11:29:10 +0530

    updated travel agent. [ have some issues with rates]

--- src/main/java/com/redberry/mvc/database/TravelAgent.java
+++ src/main/java/com/redberry/mvc/database/TravelAgent.java
@@ -22,6 +22,7 @@ public class TravelAgent {
     private String accountNo;
     private String accountName;
     private String accountBranch;
+    private String imageURL;
 
     @OneToMany (fetch = FetchType.EAGER)
     private List<Rates> rates;
@@ -30,7 +31,7 @@ public class TravelAgent {
     public TravelAgent() {
     }
 
-    public TravelAgent(String fullName, String address, String phoneNo, String email, String bankName, String accountNo, String accountName, String accountBranch, List<Rates> rates) {
+    public TravelAgent(String fullName, String address, String phoneNo, String email, String bankName, String accountNo, String accountName, String accountBranch,String imageURL, List<Rates> rates) {
         this.fullName = fullName;
         this.address = address;
         this.phoneNo = phoneNo;
@@ -39,7 +40,9 @@ public class TravelAgent {
         this.accountNo = accountNo;
         this.accountName = accountName;
         this.accountBranch = accountBranch;
+        this.setImageURL(imageURL);
         this.setRates(rates);
+
     }
 
     public int getId() {
@@ -121,5 +124,13 @@ public class TravelAgent {
 
     public void setRates(List<Rates> rates) {
         this.rates = rates;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 }
